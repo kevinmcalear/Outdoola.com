ad9048cef1e5e34929638bbb4f885897fc880d74
kevinmcalear <kevin.mcalear@gmail.com>
2014-09-14 20:23:29 -0400

    setting up working angluarfirebase qpp

---
+++ app/scripts/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name outdoolacomApp
+ * @description
+ * # outdoolacomApp
+ *
+ * Main module of the application.
+ */
+angular.module('outdoolacomApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase',
+    'firebase.utils',
+    'simpleLogin'
+  ]);
