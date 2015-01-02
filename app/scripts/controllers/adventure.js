ce13a2f6c196ed1746ae13c2295f5c053cdebe6b
kevinmcalear <kevin.mcalear@gmail.com>
2015-01-01 20:58:46 -0500

    added in the people filter

--- app/scripts/controllers/adventure.js
+++ app/scripts/controllers/adventure.js
@@ -94,4 +94,40 @@ angular.module('outdoolacomApp')
     // display any errors
     $scope.bookings.$loaded().catch(alert);
 
+
+    // Datepicker Stuff
+      $scope.today = function() {
+        return $scope.dt = new Date();
+      };
+      $scope.today();
+      $scope.showWeeks = true;
+      $scope.toggleWeeks = function() {
+        return $scope.showWeeks = !$scope.showWeeks;
+      };
+      $scope.clear = function() {
+        return $scope.dt = null;
+      };
+      $scope.disabled = function(date, mode) {
+        return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+      };
+      $scope.toggleMin = function() {
+        var _ref;
+        return $scope.minDate = (_ref = $scope.minDate) != null ? _ref : {
+          "null": new Date()
+        };
+      };
+      $scope.toggleMin();
+      $scope.open = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        return $scope.opened = true;
+      };
+      $scope.dateOptions = {
+        'year-format': "'yy'",
+        'starting-day': 1
+      };
+      $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'shortDate'];
+      $scope.format = $scope.formats[0];
+
+
   }]);
